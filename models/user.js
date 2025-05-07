48e75ccc88bbcc91aa1868478477d8c2cd30b908
TechAce <93094408+gauranshivarshney@users.noreply.github.com>
2025-05-08 00:54:38 +0530

    final

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/postdata");
+mongoose.connect(process.env.MONGO_URI);
 
 const userSchema = mongoose.Schema({
     username: String,
